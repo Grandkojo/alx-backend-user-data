132432d3ba9d0b3385fd790866a536c074dac543
Grandkojo <essienernest.kojoowus@gmail.com>
2024-05-17 19:42:33 +0000

    format data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -14,6 +14,7 @@ def filter_datum(fields: List[str], redaction: str, message: str,
                          f'{field}={redaction}{separator}', message)
     return message
 
+
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
         """
