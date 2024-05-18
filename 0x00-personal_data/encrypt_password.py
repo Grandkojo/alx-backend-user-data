790f5db094b13ba81231ca26da66a25e57fff713
Grandkojo <essienernest.kojoowus@gmail.com>
2024-05-18 11:28:42 +0000

    encrypting passwords

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,6 +7,6 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """a function to hash passwords"""
     byte_str_password = bytes(password, 'utf-8')
-    print(byte_str_password)
+    # print(byte_str_password)
     hashed = bcrypt.hashpw(byte_str_password, bcrypt.gensalt())
     return hashed
