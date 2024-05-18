b48db1ebb17cc5c8f4c0b4cc64697f056a746dd6
Grandkojo <essienernest.kojoowus@gmail.com>
2024-05-18 10:08:31 +0000

    connect ot a secure daabase

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -51,9 +51,15 @@ class RedactingFormatter(logging.Formatter):
 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
-    """Returns a connector to a database."""
-    return mysql.connector.connect(
-        user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
-        password=os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
-        host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
-        database=os.getenv('PERSONAL_DATA_DB_NAME'))
+    """Returns a connector object to a database."""
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
+    config = {
+            'user': username,
+            'password': password,
+            'host': host,
+            'database': db_name
+            }
+    return mysql.connector.connect(**config)
