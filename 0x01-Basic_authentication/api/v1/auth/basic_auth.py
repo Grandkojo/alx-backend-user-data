473b191e9eb5e4b7678d7b5bb1dff04cc2c19383
Grandkojo <essienernest.kojoowus@gmail.com>
2024-05-21 10:04:44 +0000

    base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,6 +6,7 @@
 from api.v1.auth.auth import Auth
 import base64
 
+
 class BasicAuth(Auth):
     """Basic Auth class that imports from Auth
     """
@@ -20,14 +21,16 @@ class BasicAuth(Auth):
             return None
         return authorization_header[6:]
 
-    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
         """ Decode the base64 auth string
         """
         if base64_authorization_header is None or not \
-                isinstance(base64_authorization_header , str):
+                isinstance(base64_authorization_header, str):
             return None
         try:
-            decoded_bytes = base64.b64decode(base64_authorization_header, validate=True)
+            decoded_bytes = base64.b64decode(base64_authorization_header,
+                                             validate=True)
             decoded_string = decoded_bytes.decode('utf-8')
             return decoded_string
         except (base64.binascii.Error, ValueError, UnicodeDecodeError):
