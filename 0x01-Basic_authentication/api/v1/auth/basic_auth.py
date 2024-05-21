bc41ecbe28535747a7fd945d1414180c552326d1
Grandkojo <essienernest.kojoowus@gmail.com>
2024-05-21 10:44:38 +0000

    user object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,7 +3,9 @@
 """
 
 
+from typing import TypeVar
 from api.v1.auth.auth import Auth
+from models.user import User
 import base64
 
 
@@ -50,3 +52,22 @@ class BasicAuth(Auth):
         user_name = user_credentials[0]
         password = user_credentials[1]
         return (user_name, password)
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'): #User is the current
+        """ Returns a user instance based on his email and password
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        try:
+            users = User.search({'email': user_email})
+            if not users or users == []:
+                return None
+            for user in users:
+                if user.is_valid_password(user_pwd):
+                    return user
+            return None
+        except Exception:
+            return None
