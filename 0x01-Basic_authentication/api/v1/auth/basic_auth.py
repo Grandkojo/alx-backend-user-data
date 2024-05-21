05c147205d92fbf9f9b9237e6266e658c63761a0
Grandkojo <essienernest.kojoowus@gmail.com>
2024-05-21 09:48:46 +0000

    base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
-"""Basic Authentication"""
+"""Basic Authentication
+"""
 
 
 from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
-    """Basic Auth class that imports from Auth"""
-    pass
+    """Basic Auth class that imports from Auth
+    """
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """ Extract the base64 auth header
+        """
+        if authorization_header is None or not \
+                isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic '):
+            return None
+        return authorization_header[6:]
