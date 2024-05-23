b3042c02da7ef68516150d197c87ba5d74af6569
Grandkojo <essienernest.kojoowus@gmail.com>
2024-05-23 11:52:01 +0000

    use session id for user identification

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,6 +5,7 @@
 
 from api.v1.auth.auth import Auth
 import uuid
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -29,3 +30,10 @@ class SessionAuth(Auth):
             return None
 
         return SessionAuth.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ returns a user instance based on a cookie value
+        """
+        session_id = self.session_cookie(request)
+        user_id_for_session = self.user_id_for_session_id(session_id)
+        return User.get(user_id_for_session)
